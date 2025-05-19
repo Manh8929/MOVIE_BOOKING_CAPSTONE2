1c838573d7e6abccc947d9585723d29ecadd0094
Sung23092003 <sung23092003@example.com>
2025-05-19 15:09:06 +0700

    create ui chart and logic find users

--- Backend/connection_DB.js
+++ Backend/connection_DB.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("DB_Movie", "root", "123456", {
+const sequelize = new Sequelize("DB_Movie", "root", "Qaz12edc@@", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
